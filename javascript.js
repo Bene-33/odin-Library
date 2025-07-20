d1cb70afbe34fa00551a009edd31b5a2cfd7acf0
Bene-33 <82678066+Bene-33@users.noreply.github.com>
2025-07-20 15:33:56 +0200

    initital

---
+++ javascript.js
@@ -0,0 +1,16 @@
+function Book(title, author, pages, read){
+    if (!new.target) {
+        throw Error("error use new operator")
+    }
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read
+    this.info = function(){
+        return this.title+" "+this.author+", "+this.pages+", "+this.read 
+    }
+}
+
+const theHobbit = new Book("The Hobbit", "by J.R.R. Tolkien", "295 pages", "not read yet")
+
+console.log(theHobbit.info())
