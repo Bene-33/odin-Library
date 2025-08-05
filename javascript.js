9503955712c4e5dda8f4d5a95319b6bb27536907
Bene-33 <82678066+Bene-33@users.noreply.github.com>
2025-08-05 21:13:13 +0200

    implemented feature to update myLibrary

--- javascript.js
+++ javascript.js
@@ -1,3 +1,4 @@
+// basic library functions
 const myLibrary = [];
 
 function book(title, author, pages, read, ID){
@@ -17,44 +18,59 @@ function addBookToLibrary(title, author, pages, read, ID) {
 
 };
 
+// temp ------------------------------------------------------------------------------
+// addBookToLibrary("The Hobbit", "J.R.R. Tolkien", "295", "yes")
+// addBookToLibrary("The Lord of the Rings", "J.R.R. Tolkien", "1238", "yes")
+// addBookToLibrary("No Logo!", "Naomi Klein", "295", "yes")
+// addBookToLibrary("The Witcher - the Last Wish", "Andrzej Sapkowski", "288", "no")
+// temp end ------------------------------------------------------------------------------
+
+//display new book to library
+function displayLibrary(){
+    for (let i = 0; i < book.length; i++){
+        const books = document.querySelector(".books");
+        const bookContainer = document.createElement("div");
+        const title = document.createElement("div");
+        const author = document.createElement("div");
+        const pages = document.createElement("div");
+        const read = document.createElement("div");
+        bookContainer.classList.add("bookContainer");
+        author.classList.add("booksAuthor");
+        pages.classList.add("booksPages");
+        read.classList.add("booksRead");
+        title.textContent = "Title: " + myLibrary[i].title;
+        author.textContent = "Autohr: " + myLibrary[i].author;
+        pages.textContent = "Pages: " + myLibrary[i].pages;
+        read.textContent = "Read? " + myLibrary[i].read;
+        books.appendChild(bookContainer);
+        bookContainer.appendChild(title);
+        bookContainer.appendChild(author);
+        bookContainer.appendChild(pages);
+        bookContainer.appendChild(read);
+    };
+};
+
+//modal functions to input new books
 const newBookModal = document.querySelector(".newBookModal");
 const newBookButton = document.querySelector("#newBookButton");
 const closeBookModal = document.querySelector(".closeButton");
+const submitNewBook = document.querySelector('button[type="submit"]');
 
 newBookButton.onclick = () => {
     newBookModal.showModal();
 };
 
-closeBookModal.onclick = () =>{
+closeBookModal.onclick = () => {
     newBookModal.close();
 };
 
-// temp ------------------------------------------------------------------------------
-addBookToLibrary("The Hobbit", "J.R.R. Tolkien", "295", "yes")
-addBookToLibrary("The Lord of the Rings", "J.R.R. Tolkien", "1238", "yes")
-addBookToLibrary("No Logo!", "Naomi Klein", "295", "yes")
-addBookToLibrary("The Witcher - the Last Wish", "Andrzej Sapkowski", "288", "no")
-console.log(myLibrary)
-// temp end ------------------------------------------------------------------------------
+submitNewBook.onclick = () => {
+    const bookTitle = document.querySelector('input[name="bookTitle"]').value;
+    const bookAuthor = document.querySelector('input[name="bookAuthor"]').value;
+    const bookPages = document.querySelector('input[name="bookPages"]').value;
+    const bookRead = document.querySelector('input[name="bookRead"]').checked;
 
-for (let i = 0; i < book.length-1; i++){
-    const books = document.querySelector(".books");
-    const bookContainer = document.createElement("div");
-    const title = document.createElement("div");
-    const author = document.createElement("div");
-    const pages = document.createElement("div");
-    const read = document.createElement("div");
-    bookContainer.classList.add("bookContainer");
-    author.classList.add("booksAuthor");
-    pages.classList.add("booksPages");
-    read.classList.add("booksRead");
-    title.textContent = "Title: " + myLibrary[i].title
-    author.textContent = "Autohr: " + myLibrary[i].author;
-    pages.textContent = "Pages: " + myLibrary[i].pages;
-    read.textContent = "Read? " + myLibrary[i].read;
-    books.appendChild(bookContainer);
-    bookContainer.appendChild(title);
-    bookContainer.appendChild(author);
-    bookContainer.appendChild(pages);
-    bookContainer.appendChild(read);
+    addBookToLibrary(bookTitle, bookAuthor, bookPages,bookRead);
+    event.preventDefault();
+    displayLibrary();
 };
